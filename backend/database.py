037f3f48761e665901d4cf33827a6041b12da805
Giovanni Spadaccini <giospadaccini74@gmail.com>
2024-03-23 23:01:13 +0100

    new database

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sportactivities.db"
+
+
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL', "sqlite:///./sportactivities.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
